5632093f679dc322f4d54ebee484ffc4728b4a21
Jonathan Strandberg <jonst341@student.liu.se>
2018-02-08 21:17:31 +0100

    storbygge

--- systemc/tl/tl.cc
+++ systemc/tl/tl.cc
@@ -1,25 +1,66 @@
+#include <iostream>
 #include "tl.h"
+
+using std::cout;
+using std::endl;
 
 tl::tl(sc_module_name name)
 : sc_module(name)
 {
-  SC_METHOD(get_sensor);
-  SC_METHOD(check_others);
-  SC_METHOD(print_light);
-  //ta in sensor
-  //kolla om ledigt
-  //skriv ut lampa eller vänta
+
+  SC_METHOD(get_sensors);
+  dont_initialize();
+  sensitive  << inc_NS << inc_SN << inc_WE << inc_EW;
+
+  SC_METHOD(traffic_logic);
+  dont_initialize();
+
+  SC_METHOD(print_lights);
+  dont_initialize();
+  //Trigga på sensorförändring: inc_EW inc_NS inc_SN inc_WE
+  //kontrollera nuvarande status
+  //justera lampor vartefter
+  //skriv ut
 }
-void tl::get_sensor()
+void tl::get_sensors()
 {
-  bool sensorValue = //sensor -> read();
+  inc[0] = inc_NS->read();
+  inc[1] = inc_SN->read();
+  inc[2] = inc_WE->read();
+  inc[3] = inc_EW->read();
+
+  traffic_logic();
 }
-void tl::check_others()
+void tl::traffic_logic()
 {
-  bool good2Go = //controlInput -> read();
+  //NS || SN  om någon av dem inte redan är true samtidigt som WE || EW är true
+  if ((inc[0] || inc[1]) && !((tl[0] || tl[1]) && (inc[2] || inc[3])))
+  {
+    tl[0] = inc[0];
+    tl[1] = inc[1];
+    tl[2] = false;
+    tl[3] = false;
+  }
+  else if(inc[2] || inc[3]) //EW || WE :==D q p
+  {
+    tl[0] = false;
+    tl[1] = false;
+    tl[2] = inc[2];
+    tl[3] = inc[3];
+  }
+  else
+  {
+    tl[0] = false;
+    tl[1] = false;
+    tl[2] = false;
+    tl[3] = false;
+  }
+  print_lights();
 }
-void tl::print_light
+void tl::print_lights()
 {
-  bool light; //true grön, false röd
-  cout << grön / röd
+  cout << endl << "NS : " << tl[0];
+  cout << endl << "SN : " << tl[1];
+  cout << endl << "WE : " << tl[2];
+  cout << endl << "EW : " << tl[3] << endl;
 }
---
+++ systemc/tl/tl.h
@@ -0,0 +1,31 @@
+#ifndef TL_H
+#define TL_H
+#include <systemc.h>
+
+SC_MODULE(tl)
+{
+  //boolkanaler för detektion av inkommande bilar
+  sc_in<bool> inc_NS;
+  sc_in<bool> inc_SN;
+  sc_in<bool> inc_WE;
+  sc_in<bool> inc_EW;
+
+  //boolkanaler för ljus (används ej atm)
+  sc_out<bool> tl_NS;
+  sc_out<bool> tl_SN;
+  sc_out<bool> tl_WE;
+  sc_out<bool> tl_EW;
+
+  //internt
+  bool tl[4] = {false,false,false,false};
+  bool inc[4] = {false,false,false,false};
+
+  SC_HAS_PROCESS(tl);
+  tl(sc_module_name name);
+
+  void get_sensors();
+  void print_lights();
+  void traffic_logic();
+}
+
+#endif //TL_H
